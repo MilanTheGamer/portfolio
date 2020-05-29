83d6de4b9a69a5bc0c3d5c7c72ebe35ef8feaa6f
MilanTheGamer <milanthegamer@gmail.com>
2020-05-29 20:27:06 +0530

    responsive js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const expanded = document.getElementById("expanded");
+const profileInfo = document.getElementById("profileInfo");
+const contacts = document.getElementById("contacts");
 
 const knowMore = () => {
     expanded.scrollIntoView();
 };
+
+profileInfo.addEventListener("click",()=>{
+    contacts.scrollIntoView();
+})
